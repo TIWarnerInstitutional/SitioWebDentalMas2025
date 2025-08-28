a8f5ccf7a8d0644f16d807c1f4d5e4294abbd22f
TIWarnerInstitutional <areati@warnerinstitutional.com>
2025-08-28 13:57:11 -0600

    Agrega SpeedInsights y Analytics de Vercel en _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import CookieAlert from '../components/CookieAlert';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,6 +12,8 @@ export default function App({ Component, pageProps }) {
       </Head>
       <Component {...pageProps} />
       <CookieAlert />
+  <SpeedInsights />
+  <Analytics />
     </>
   );
 }
